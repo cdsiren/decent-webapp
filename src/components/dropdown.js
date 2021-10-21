91c3175c97d97bd8bf284ced42430a3a06788d97
cdsiren <correct-charlie@decent.xyz>
2021-10-21 14:00:05 -0400

    dropdown commit on 'main'

--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -5,13 +5,14 @@ const Dropdown = ({ isOpen, toggle }) => {
     return (
         <div className={
             isOpen 
-                ? "grid grid-rows-4 text-center items-center bg-white text-purple-700 font-Headers" : "hidden"
+                ? "grid grid-rows-4 text-center items-center bg-white text-purple-700 font-Headers z-50 relative" : "hidden"
         }
         onClick={toggle}
         >
-            <Link className="p-4" to="/faq">FAQ</Link>
-            <Link className="p-4" to="/blog">Artist Blog</Link>
-            <a className="my-button" href="https://form.typeform.com/to/Xkh00RVd">Join Waitlist</a>
+            <Link className="p-4 font-Headers" to="/">Home</Link>
+            <Link className="p-4 font-Headers" to="/faq">FAQ</Link>
+            <Link className="p-4 font-Headers" to="/blog">Artist Blog</Link>
+            <a className="my-button font-Headers" href="https://form.typeform.com/to/Xkh00RVd">Join Waitlist</a>
         </div>
     );
 }
