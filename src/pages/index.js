2b7be009c5ebc956046c78da1bfe669a82e5d13b
cdsiren <correct-charlie@decent.xyz>
2021-10-21 13:37:22 -0400

    commit on 'main'

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Hero from '../components/hero';
 import Content from '../components/content';
-//import MovingBlob from '../components/MovingBlob';
 
 const Home = () => {
     return (
