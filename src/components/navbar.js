cea69be2fbb87d3b0837501d281a0f31948a1233
cdsiren <correct-charlie@decent.xyz>
2021-10-27 23:23:25 -0400

    commit on 'main'

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -33,6 +33,7 @@ const navbar = ({ toggle }) => {
                 <Link className="p-4" to="/faq">FAQ</Link>
                 <a className="p-4" href="https://mirror.xyz/0x673Fc290d919684A1B1E46AA69152f7b56FBADD8/qsvUuJOGdjj_o49zqtIaSx8wh-92usgJMkkomv-LHNs">Blog</a>
                 <Link className="my-button" to="/waitlist">Join Waitlist</Link>
+                <Link className="wallet-button" to="/waitlist">Connect Wallet</Link>
             </div>   
         </nav>
     );
