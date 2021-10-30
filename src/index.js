b65a1e0fd97ac66ad448bd9868012efb83c4f0d6
cdsiren <correct-charlie@decent.xyz>
2021-10-30 14:13:55 -0400

    commit on 'main'

--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
+import {DAppProvider} from '@usedapp/core';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+    {/* 
+       Wrap our app in the provider, config is required, 
+        but can be left as an empty object: 
+    */}
+    <DAppProvider config={{}}>
       <App />
+    </DAppProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
